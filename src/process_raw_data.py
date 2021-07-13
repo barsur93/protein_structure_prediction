63ada50e6a166c5205e46110934187e56a91a755
barsur93 <bartlomiej.surpeta@gmail.com>
2021-07-14 00:10:34 +0200

    processing update

--- src/process_raw_data.py
+++ src/process_raw_data.py
@@ -1,5 +1,6 @@
 import gzip
 import csv
+import pandas as pd
 
 
 class Sequences:
@@ -9,7 +10,7 @@ class Sequences:
     #     self.sequence = sequence
     #     self.secondary_str = secondary_str
     #
-    def process_raw_sequences(self, raw_sequences_file):
+    def process_raw_sequences(self, raw_sequences_file: str):
         output_csv = raw_sequences_file.replace('txt.gz', 'csv')
         with gzip.open(filename=raw_sequences_file, mode='rt') as input_file:
             with open(output_csv, 'wt') as output_file:
@@ -46,14 +47,27 @@ class Sequences:
                     if len(sequence) == len(secondary_str):
                         csv_writer.writerow([pdb_id, chain, sequence, secondary_str])
 
-                        if i % 10000 == 0:
-                            print(i)
                     else:
                         raise ValueError('The lengths of sequence and secondary structure does not match!')
 
         print(f'processed {i} sequences')
+        sequences_df = pd.read_csv(output_csv)  # converting to DataFrame here due to lower efficiency to writerow()
+        return sequences_df
+
+    def combine_pdb_pisces(self, sequences_df: pd.DataFrame, pisces_file: str):
+        pisces_df = pd.read_csv(pisces_file, sep='\t')
+
+        return pisces_df
+
+    def clean_sequences(self, sequences_df: pd.DataFrame):
+        pass
 
 
-processing = Sequences()
+# processing = Sequences()
+# 
+# seq_df = processing.process_raw_sequences(raw_sequences_file='../../data/2021-07-09-ss.txt.gz')
+# print(seq_df.head())
+# 
+# pisces_df = processing.combine_pdb_pisces(seq_df, '../../data/cullpdb_pc30_res2.0_R0.25_d2021_07_02_chains10870.gz')
+# print(pisces_df.head(1))
 
-processing.process_raw_sequences('../../data/2021-07-09-ss.txt.gz')
