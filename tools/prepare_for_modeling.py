871a58cd925d919ce2928d7ad3f495c778433a6b
barsur93 <bartlomiej.surpeta@gmail.com>
2021-08-06 00:54:56 +0200

    parse protvec embeddings

--- tools/prepare_for_modeling.py
+++ tools/prepare_for_modeling.py
@@ -3,6 +3,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
 import numpy as np
 import pandas as pd
+from typing import Tuple
 
 
 #  function inspired from https://www.kaggle.com/helmehelmuto/secondary-structure-prediction-with-keras
@@ -18,7 +19,8 @@ def tokenize_inputs(input_sequences: np.array, maxlen: int) -> np.array:
     input_tokenizer.fit_on_texts(input_sequences)
     x_seq = input_tokenizer.texts_to_sequences(input_sequences)
     x_seq = pad_sequences(x_seq, maxlen=maxlen, padding='post')
-    return x_seq
+    seq_index = input_tokenizer.word_index
+    return x_seq, seq_index
 
 
 def tokenize_target(target_sst: pd.Series, maxlen: int) -> np.array:
@@ -26,5 +28,19 @@ def tokenize_target(target_sst: pd.Series, maxlen: int) -> np.array:
     target_tokenizer.fit_on_texts(target_sst)
     y_sst = target_tokenizer.texts_to_sequences(target_sst)
     y_sst = pad_sequences(y_sst, maxlen=maxlen, padding='post')
+    sst_index = target_tokenizer.word_index
     y_sst = to_categorical(y_sst)
-    return y_sst
+    return y_sst, sst_index
+
+
+def parse_protvec_embeddings(file: str) -> Tuple[tuple, dict]:
+    embeddings = dict()
+    with open(file, 'r') as f:
+        content = f.readlines()
+        embeddings_shape = tuple(int(x) for x in content[0].split())
+        for line in content[1:]:
+            values = line.split()
+            ngram = values[0].upper()
+            coefs = np.array(values[1:], dtype='float32')
+            embeddings[ngram] = coefs
+    return embeddings_shape, embeddings
